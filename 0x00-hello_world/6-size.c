cce40bad7f11bcf4a7e3f1d96cb9c0e9ca83cdc0
Salomon Fondop <Salomonfondop@gmail.com>
2022-12-08 11:23:20 -0800

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,6 @@ int main(void)
 	printf("Size of an int: %lu byte(S)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %lu byte(S)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %lu byte(S)\n", (unsigned long)sizeof(d));
-	printf("size of a char: %lu byte(S)\n", (unsigned long)sizeof(f));
+	printf("size of a float: %lu byte(S)\n", (unsigned long)sizeof(f));
 	return (0);
 }
