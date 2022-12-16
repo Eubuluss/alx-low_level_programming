2e63a9a14ba45404f70a76e90628f5ff4e0d2c0c
Salomon Fondop <Salomonfondop@gmail.com>
2022-12-15 17:08:27 -0800

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,13 @@
 /**
  * _isupper - checks if a  character is uppercase or not
  * @c: character to be tested
- * Return: 1 whether it is, 0 otherwise
+ * Return: 1 if it is an uppercase character, 0 otherwise
  */
 
 int _isupper(int c)
 
 {
-	if ((c >= 'A') && (c <= 'Z'))
+	if ((c >= '65') && (c <= '90'))
 	{
 		return (1);
 	}
@@ -17,3 +17,4 @@ int _isupper(int c)
 	{
 		return (0);
 	}
+}
