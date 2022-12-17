e2b58200629f1eb807c15737f54ac2484caa7c79
Salomon Fondop <Salomonfondop@gmail.com>
2022-12-16 16:17:24 -0800

    tasks

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -13,5 +12,4 @@ int _putchar(char);
 int _isupper(int);
 int _isdigit(int);
 int mul(int, int);
-
 #endif
